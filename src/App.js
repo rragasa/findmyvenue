f50a536eefb8035f65f28b8cdf244c0e40182ef5
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-08-11 17:44:12 +0100

    Storing venues in redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchVenues } from './utils/actions/appActions';
 import Typography from '@material-ui/core/Typography';
 import Search from './components/Search';
+import {
+  apiVersion as v,
+  client_id,
+  client_secret,
+} from './config.json';
 
-const App = () => {
-  return (
-    <Fragment>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Find my venue
-      </Typography>
-      <div>
-        <Search />
-    </div>
-    </Fragment>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(location) {
+    const { getVenues } = this.props;
+    const payload = {
+      v,
+      client_id,
+      client_secret,
+      near: location,
+    };
+
+    getVenues(payload);
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <header className="App-header">
+       <Typography variant="h3" component="h3">Find my venue</Typography>
+       </header>
+       <Search
+        onSubmit={(location) => this.handleSubmit(location)}
+        />
+      </div>
+     );
+  }
 }
+const mapStateToProps = ({
+  appReducer: {
+    venues,
+  },
+ }) => ({
+   venues,
+});
 
-export default App;
+ const mapDispatchToProps = dispatch => ({
+  getVenues: (payload) => dispatch(fetchVenues(payload))
+ })
+
+ export default connect(mapStateToProps, mapDispatchToProps)(App);
